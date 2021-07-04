02fbd201b261a405623d6ce39b0897f4c6b0c00d
Aamir Bashir <ksaak33@gmail.com>
2021-07-04 12:17:06 +0530

    Using CSS to style login Page

--- login.php
+++ login.php
@@ -5,42 +5,40 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./assets/images/favicon.ico">
+
+  <link rel="stylesheet" href="./assets/css/loginstyle.css">
   <title>Login Page</title>
 </head>
 
 <body>
-  <h1>Login Page</h1>
 
-  <h2>GET FORM EXAMPLE</h2>
-  <form action="" method="get">
-    <input type="text" name="username" id="" placeholder="Enter your username">
-    <input type="submit" value="Login">
-  </form>
+  <div class="container-div">
+    <h1>Login Page</h1>
 
-  <h2>POST FORM EXAMPLE</h2>
-  <form action="" method="post">
-    <input type="text" name="username" id="" placeholder="Enter your username">
-    <input type="submit" value="Login">
-  </form>
+    <h2 class="center-text">POST FORM EXAMPLE</h2>
+    <form action="" method="post">
+      <input type="text" name="username" id="" placeholder="Enter your username">
 
+      <input type="password" name="password" id="" placeholder="Enter your password">
+      <input type="submit" value="Login">
+    </form>
+  </div>
   <?php
 
-  if (isset($_GET['username'])) {
-    echo "<pre>";
-    echo "GET Global Varaible DATA : ";
-    print_r($_GET);
-    echo "</pre>";
-  }
-
   if (isset($_POST['username'])) {
-    echo "POST Global Varaible DATA : ";
+
     print_r($_POST);
+    echo "<br>";
+    $username = $_POST["username"];
+    $password = $_POST['password'];
+
+
+    echo "Your inputed username is : " . $username;
+    echo "<br>";
+
+    echo "Your inputed password is : " . $password;
   }
-
-
-  // $strGreeting = "Hello world, im being presented via php code.";
-
-  // echo $strGreeting;
   ?>
 
 </body>
