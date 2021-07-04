a19c9b48ca10de1772c0b4cd54148e6ed316ba42
Aamir Bashir <ksaak33@gmail.com>
2021-07-04 18:49:18 +0530

    Using CSS to style the login page

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="./assets/images/favicon.ico">
 
-  <link rel="stylesheet" href="./assets/css/loginstyle.css">
+  <link rel="stylesheet" href="./assets/css/login-style.css">
   <title>Login Page</title>
 </head>
 
@@ -18,26 +18,69 @@
 
     <h2 class="center-text">POST FORM EXAMPLE</h2>
     <form action="" method="post">
-      <input type="text" name="username" id="" placeholder="Enter your username">
-
-      <input type="password" name="password" id="" placeholder="Enter your password">
-      <input type="submit" value="Login">
+      <input class="input-tag" type="text" name="username" id="" placeholder="Enter your username">
+      <br>
+      <input class="input-tag" type="password" name="password" id="" placeholder="Enter your password">
+      <br>
+      <input class="button" type="submit" value="Login">
     </form>
   </div>
   <?php
+  /**
+   * Display login to user 
+   * After submission we collect the data IN GET or POST array
+   * 
+   * From Database we get user data.
+   * 
+   * We compare the submited data with our database data 
+   *  If it matches then user is logged in else we show invalid user message.
+   *  
+   */
+
+  $userData = [
+    [
+      "username" => "ummer",
+      "password" => "abcd"
+    ],
+    [
+      "username" => "mirshahid",
+      "password" => "qwerty"
+    ]
+  ];
+  echo "<pre>";
+  //print_r($userData);
+  // echo "</pre>";
 
   if (isset($_POST['username'])) {
 
-    print_r($_POST);
-    echo "<br>";
     $username = $_POST["username"];
     $password = $_POST['password'];
 
+    $boolUserExists = false;
 
-    echo "Your inputed username is : " . $username;
-    echo "<br>";
+    foreach ($userData as $user) {
+      //print_r($user);
+      //echo $user["username"] . "<br>";
+      if ($user["username"] == $username && $user["password"] == $password) {
+        $boolUserExists = true;
+      }
+    }
 
-    echo "Your inputed password is : " . $password;
+    if ($boolUserExists) {
+      echo "User is logged in";
+      header("Location:./dashboard.php");
+    } else {
+      echo "Username or password is incorrect!";
+    }
+
+    //print_r($_POST);
+    //echo "<br>";
+
+
+    // echo "Your inputed username is : " . $username;
+    // echo "<br>";
+
+    // echo "Your inputed password is : " . $password;
   }
   ?>
 
